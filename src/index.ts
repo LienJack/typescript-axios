e05f7838dd3f9224eacbdd24917ab96fc3ee96d7
lienjie <longyuhuhao@126.com>
2019-08-28 17:55:53 +0800

    1.响应数据解析

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/header'
 function axios(config:AxiosRequestConfig): AxiosPromise {
   // todo
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config:AxiosRequestConfig): void {
@@ -26,5 +28,10 @@ function transformRequestData (config: AxiosRequestConfig): any {
 function transformRequsetHeaders (config: AxiosRequestConfig): any {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData (res: AxiosResponse) {
+  res.data = transformResponse(res.data)
+  return res
 }
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { request } from "https";
+
 export type Method = 'get' | 'GET'
 |  'delete' | 'Delete'
 | 'head' | 'HEAD'
@@ -12,6 +14,7 @@ export interface AxiosRequestConfig {
   params ?: any ,
   headers ?: any ,
   responseType ?: XMLHttpRequestResponseType ,
+  timeout?: number
 }
 
 export interface AxiosResponse {
@@ -24,3 +27,11 @@ export interface AxiosResponse {
 }
 
 export interface AxiosPromise extends Promise<AxiosResponse>{}
+
+export interface AxiosError extends Error {
+  isAxiosError: boolean,
+  config: AxiosRequestConfig,
+  code?:string | null,
+  request?: any,
+  response: AxiosResponse
+}
